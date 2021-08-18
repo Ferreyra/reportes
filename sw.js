8d641dddcb8748d7810bb7d2fef3a1545a1c337a
Ferreyra <robfercor@gmail.com>
2021-08-17 23:37:50 -0500

    Update sw.js
    OffCanvas fix, cdn jspdf

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const STATIC_CACHE    = 'static-v1';
-const DYNAMIC_CACHE   = 'dynamic-v1';
-const INMUTABLE_CACHE = 'inmutable-v1';
+const STATIC_CACHE    = 'static-v2';
+const DYNAMIC_CACHE   = 'dynamic-v2';
+const INMUTABLE_CACHE = 'inmutable-v2';
 
 const APP_SHELL = [
   '/',
@@ -13,7 +13,8 @@ const APP_SHELL = [
 const APP_SHELL_INMUTABLE = [
   'archivos/bootstrap.min.css',
   'archivos/bootstrap.min.js',
-  'archivos/jspdf.min.js'
+  'archivos/jspdf.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.3.1/jspdf.umd.min.js'
 ];
 
 function actCacheDinamico( dynamicCache, req, res ) {
